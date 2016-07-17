568c4880412a24edeb5b9765efa15549c96828e5
Mikael Karon <mikael@karon.se>
2016-07-17 23:33:37 +0200

    Always use experimentId from data-experiment-id attribute

--- jquery.js
+++ jquery.js
@@ -1,28 +1,50 @@
 jQuery(function($) {
-  $("body")
+  var noop = function() {};
+  var getExperimentId = function() {
+    return $(this.parents("[data-experiment-id]").addBack("[data-experiment-id]").get().reverse()).data("experimentId");
+  };
+
+  $("html")
+    .on("ready.component", "[data-components~='experiment']", function($event, callback, errback) {
+      $($event.target)
+        .find("[data-experiment-variation]")
+        .addBack("[data-experiment-variation]")
+        .each(function(index, element) {
+          var $element = $(element);
+
+          try {
+            (callback || noop)($element
+              .trigger("initialize.component", "experiment")
+              .trigger("chooseVariation.experiment")
+              .trigger("start.component", "experiment"));
+          } catch (e) {
+            (errback || noop)(e, $element);
+          }
+        });
+    })
     .on({
-      "experiment:chooseVariation": function($event, experimentId) {
+      "chooseVariation.experiment": function($event) {
         var $target = $($event.target);
+        var experimentId = getExperimentId.call($target);
 
         ga("experiment:chooseVariation", experimentId, function(variation) {
-          $target.trigger("experiment:variationChosen", [variation]);
+          $target.trigger("variationChosen.experiment", [variation]);
         });
       },
 
-      "experiment:variationChosen": function($event, variation) {
+      "setChosenVariation.experiment": function($event, variation) {
         var $target = $($event.target);
+        var experimentId = getExperimentId.call($target);
 
-        $target.trigger("experiment:matchesVariation", $target.data("experimentVariation") === variation);
+        ga("experiment:setChosenVariation", experimentId, variation, function() {
+          $target.trigger("variationChosen.experiment", [variation]);
+        });
       },
 
-      "experiment:matchesVariation": function($event, matches) {
-        $($event.target).attr("data-experiment-matches", matches);
-      }
-    })
-    .find("[data-experiment-variation]")
-    .each(function(index, element) {
-      var $element = $(element);
+      "variationChosen.experiment": function($event, variation) {
+        var $target = $($event.target);
 
-      $element.trigger("experiment:chooseVariation", [$($element.parents("[data-experiment-id]").addBack("[data-experiment-id]").get().reverse()).data("experimentId")]);
+        $($event.target).attr("data-experiment-variation-chosen", variation);
+      }
     });
 });
