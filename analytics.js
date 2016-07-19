66f1c84480bc479fcf98a9f79632e75ed5940422
Mikael Karon <mikael@karon.se>
2016-07-20 00:16:06 +0200

    Don't expose Experiment.prototype.do

--- analytics.js
+++ analytics.js
@@ -3,19 +3,19 @@
     return;
   }
 
+  var _cxApiProxy = function(experimentId, methodName, args, config, callback, errback) {
+    return w[w["_cxApiObject"] || "_cxApi"].apply(this, [experimentId, methodName, args, config || this.config, callback, errback]);
+  };
+
   var Experiment = w[_cxApiPlugin] = function(tracker, config) {
     this.tracker = tracker;
     this.config = config;
   };
 
-  Experiment.prototype.do = function(experimentId, methodName, args, config, callback, errback) {
-    return w[w["_cxApiObject"] || "_cxApi"].apply(this, [experimentId, methodName, args, config || this.config, callback, errback]);
-  };
-
   Experiment.prototype.chooseVariation = function(experimentId, config, callback, errback) {
     var self = this;
 
-    return self.do(experimentId, "chooseVariation", [], config, function(result) {
+    return _cxApiProxy.call(self, experimentId, "chooseVariation", [], config, function(result) {
       self.tracker.send({
         "hitType": "event",
         "nonInteraction": true,
@@ -35,7 +35,7 @@
   Experiment.prototype.setChosenVariation = function(experimentId, chosenVariation, config, callback, errback) {
     var self = this;
 
-    return self.do(experimentId, "setChosenVariation", [chosenVariation], config, function(result) {
+    return _cxApiProxy.call(self, experimentId, "setChosenVariation", [chosenVariation], config, function(result) {
       self.tracker.send({
         "hitType": "event",
         "nonInteraction": true,
@@ -53,11 +53,11 @@
   };
 
   Experiment.prototype.getChosenVariation = function(experimentId, config, callback, errback) {
-    return this.do(experimentId, "getChosenVariation", [], config, callback, errback);
+    return _cxApiProxy.call(this, experimentId, "getChosenVariation", [], config, callback, errback);
   };
 
   Experiment.prototype.cxApi = function(experimentId, config, callback, errback) {
-    return this.do(experimentId, "cxApi", [], config, callback, errback);
+    return _cxApiProxy.call(this, experimentId, "cxApi", [], config, callback, errback);
   };
 
   w[w["GoogleAnalyticsObject"] || "ga"]("provide", "experiment", Experiment);
